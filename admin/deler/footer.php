c266c407f9de06affb6efc6470db4f823ca5aab4
Salahudin Ahmad <112163291+SalahTheScripter@users.noreply.github.com>
2023-06-03 13:52:27 +0200

    Delete admin directory

--- admin/deler/footer.php
+++
@@ -1,9 +0,0 @@
-       <!-- Footer Seksjon Starter -->
-       <div class="footer text-center">
-            <div class="innpakning">
-                Footer
-            </div>
-        </div>
-        <!-- Footer Seksjon Slutter -->
-    </body>
-</html>
--- admin/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // Inkludere konstanter.php for url
-    include('../konfig/konstanter.php');
-    // 1. Ødelegge økten
-    session_destroy(); // Unsets $_SESSION['bruker]
-
-    // 2. Omdirigere til Login-siden
-    header('location:'.NETTSTEDURL.'admin/login.php');
-?>
